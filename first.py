071ad246c531f6c4b34791cc3d21509c60bc9233
priyanka <priyanka123@gmail.com>
2026-02-19 09:15:38 +0530

    username and email will give

---
+++ first.py
@@ -0,0 +1,9 @@
+a=int(input("enter the number:"))
+p=0
+if a<=1:
+    print("number is a less then 1")
+for i in range(2,a):
+    if n%2==0:
+        print("number is not prime:",a)
+    else:
+        print("number is prime :",a)        
